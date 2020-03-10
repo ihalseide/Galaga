2f49aabd23f15a31ccd7748825376e540c5713f0
Izak Halseide <halseide.izak@gmail.com>
2020-03-09 22:11:28 -0600

    Add setup.py
    This is a python manifest file that will make dependencies clear in the future

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="galaga-ihalseide",
+    version="0.0.1",
+    author="Izak Halseide",
+    author_email="halseide.izak@gmail.com",
+    description="A clone of the game Galaga",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ihalseide/Galaga",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: GNU General Public License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
